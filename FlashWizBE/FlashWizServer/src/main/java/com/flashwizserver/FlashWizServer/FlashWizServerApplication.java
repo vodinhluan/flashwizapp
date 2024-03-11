21acea7f5918a2dd51b886bcba353ddcf1b765c0
vodinhluan <luandinhvo2002@gmail.com>
2024-03-11 16:36:48 +0700

    init backend

---
+++ FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/FlashWizServer/FlashWizServerApplication.java
@@ -0,0 +1,13 @@
+package com.flashwizserver.FlashWizServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashWizServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashWizServerApplication.class, args);
+	}
+
+}
---
+++ FlashWizBE/FlashWizServer/src/test/java/com/flashwizserver/FlashWizServer/FlashWizServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashwizserver.FlashWizServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashWizServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
