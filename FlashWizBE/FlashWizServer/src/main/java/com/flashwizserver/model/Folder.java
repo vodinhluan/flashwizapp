f3d031fcccc4a47aea76a7307be701106bd88a75
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-03-11 22:50:20 +0700

    Create Folder Model

---
+++ FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/model/Folder.java
@@ -0,0 +1,59 @@
+package com.flashwizserver.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "folder")
+public class Folder {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer id;
+	@Column(length = 128, nullable = false)
+	private String name;
+	@Column(length = 128, nullable = false)
+	private String descriptions;
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+//	@ManyToMany(fetch = FetchType.EAGER)
+//	@JoinTable(name = "folder_flashcard", joinColumns = @JoinColumn(name = "folder_id"), inverseJoinColumns = @JoinColumn(name = "flashcard_id"))
+//	private Set<Flashcard> flashcard = new HashSet<>();
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescriptions() {
+		return descriptions;
+	}
+
+	public void setDescriptions(String descriptions) {
+		this.descriptions = descriptions;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
