2a38f9700479536a1f01ff5419395c123c58dc42
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-03-13 14:18:16 +0700

    Create Model/Repo Flashcard

--- FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/model/Flashcard.java
+++ FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/model/Flashcard.java
@@ -1,16 +1,23 @@
 package com.flashwizserver.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "folder")
-public class Folder {
+@Table(name = "flashcard")
+public class Flashcard {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer id;
@@ -21,9 +28,17 @@ public class Folder {
 	@ManyToOne
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "folder_flashcard", joinColumns = @JoinColumn(name = "folder_id"), inverseJoinColumns = @JoinColumn(name = "flashcard_id"))
-	private Set<Flashcard> flashcard = new HashSet<>();
+	@ManyToMany(mappedBy = "flashcard")
+	private List<Folder> folders;
+
+	public List<Folder> getFolders() {
+		return folders;
+	}
+
+	public void setFolders(List<Folder> folders) {
+		this.folders = folders;
+	}
+
 	public Integer getId() {
 		return id;
 	}
