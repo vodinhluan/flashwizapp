fdb117e5ab36d40fe8424812378dd7446fa4c97e
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-03-16 12:33:48 +0700

    [FIX] Register Un-Successfully 1

--- FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/controller/UserController.java
+++ FlashWizBE/FlashWizServer/src/main/java/com/flashwizserver/controller/UserController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+//import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,26 +29,26 @@ public class UserController {
 		userDAO.saveUser(user);
 	}
 	
-	@PostMapping("/user/register")
-    public ResponseEntity registerNewUser(@RequestParam("name") String name,
-                                          @RequestParam("email")String email,
-                                          @RequestParam("password")String password){
-
-        if(name.isEmpty() || email.isEmpty() || password.isEmpty()){
-            return new ResponseEntity<>("Hãy điền đầy đủ thông tin ", HttpStatus.BAD_REQUEST);
-        }
-
-        // Encrypt / Hash Password:
-        String hashed_password = BCrypt.hashpw(password, BCrypt.gensalt());
-
-        // Register New User:
-        int result = userDAO.registerNewUserServiceMethod(name, email, hashed_password);
-
-        if(result != 1){
-            return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<>("success", HttpStatus.OK);
-
-    }
+//	@PostMapping("/user/register")
+//    public ResponseEntity registerNewUser(@RequestParam("name") String name,
+//                                          @RequestParam("email")String email,
+//                                          @RequestParam("password")String password){
+//
+//        if(name.isEmpty() || email.isEmpty() || password.isEmpty()){
+//            return new ResponseEntity<>("Hãy điền đầy đủ thông tin ", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        // Encrypt / Hash Password:
+//        String hashed_password = BCrypt.hashpw(password, BCrypt.gensalt());
+//
+//        // Register New User:
+//        int result = userDAO.registerNewUserServiceMethod(name, email, hashed_password);
+//
+//        if(result != 1){
+//            return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return new ResponseEntity<>("success", HttpStatus.OK);
+//
+//    }
 }
