46ea2f1714224c0eeb91b02937935ac97b3f011e
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-03-23 10:46:16 +0700

    [UPDATE] SignUp, Login to HomePage and fix version Android

--- FlashWiz_FrontEnd/app/src/main/java/com/example/flashwiz_fe/presentation/screen/StatisticScreen.kt
+++ FlashWiz_FrontEnd/app/src/main/java/com/example/flashwiz_fe/presentation/screen/StatisticScreen.kt
@@ -1,2 +1,20 @@
 package com.example.flashwiz_fe.presentation.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun StatisticScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        // Hiển thị nội dung của màn hình Chat
+        Text(text = "Đây là trang thống kê", modifier = Modifier.fillMaxSize())
+    }
+}
