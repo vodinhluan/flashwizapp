af199af6e8e4d4d700aad74f84c2153f02be8690
duylinh273 <duylinhvo273@gmail.com>
2024-05-02 11:03:31 +0700

    Connect Folder Retrofit

---
+++ FlashWiz_FrontEnd/app/src/main/java/com/example/flashwiz_fe/data/remote/FolderRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.example.flashwiz_fe.data
+
+import com.example.flashwiz_fe.data.remote.FolderApiService
+import com.example.flashwiz_fe.data.remote.GroupApiService
+import com.example.flashwiz_fe.domain.model.FolderDetail
+import com.example.flashwiz_fe.domain.repository.FolderRepository
+
+class FolderRepositoryImpl(private val folderApiService: FolderApiService) : FolderRepository {
+    override suspend fun getFoldersByUserId(userId: Int?) : List<FolderDetail> {
+        return folderApiService.getFoldersByUserId(userId)
+    }
+
+    override suspend fun getFolderById(folderId: Int): FolderDetail {
+        return folderApiService.getFolderById(folderId)
+    }
+}
